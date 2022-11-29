37bb97869c0bbd7b41b69e8b937023d2459bc6ec
Shasss <shaswatkumar447@gmail.com>
2022-11-29 06:25:11 +0530

    commited paymentButton

--- src/PaymentButton.js
+++ src/PaymentButton.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 export default function PaymentButton() {
+  const[amount,setamount]=useState(0)
+  const[amountpaid,setpaidamount]=useState(0)
+  const paymentHandler=e=>{
+    setamount(e.target.value);
+  }
+  const submitHandle=e=>{
+    e.preventDefault();
+    setpaidamount(amount);
+  } 
   return (
-    <div>   
-      <input type='number' placeholder="amount of ether"></input>
-    <button>pay</button>
+    <div>
+      <form onSubmit={submitHandle}>
+      <input type='number' placeholder="amount of ether" value={amount}onChange={paymentHandler}></input>
+    <button type='submit'>pay</button>
+        </form> 
+        <h2>{amountpaid}</h2> 
     </div>
   )
 }
